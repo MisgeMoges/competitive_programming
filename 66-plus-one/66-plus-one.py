8e93bb8f8a304caa05034c3a1ce4c4632f964eca
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-04 13:34:34 +0300

    Time: 39 ms (47.15%), Space: 13.8 MB (95.54%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,23 +1,25 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 1
-        n = len(digits)
-        dig = digits[0]
-        for i in range(n-1, -1, -1):
-            dig = digits[i]
-            if(dig == 9 and carry == 1):
-                dig = 0
-                carry = 1
-            else:
-                dig = dig + carry
-                carry = 0
-            digits[i] = dig
+     
+        return list(map(int, list(str(1+int("".join(list(map(str, digits))))))))
+#         carry = 1
+#         n = len(digits)
+#         dig = digits[0]
+#         for i in range(n-1, -1, -1):
+#             dig = digits[i]
+#             if(dig == 9 and carry == 1):
+#                 dig = 0
+#                 carry = 1
+#             else:
+#                 dig = dig + carry
+#                 carry = 0
+#             digits[i] = dig
 
-        if(carry == 1):
-            extra = [1]
-            digits = extra + digits
+#         if(carry == 1):
+#             extra = [1]
+#             digits = extra + digits
 
-        return digits
+#         return digits
 	
     
         
