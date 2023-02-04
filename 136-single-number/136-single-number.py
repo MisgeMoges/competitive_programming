9fb40309d9abf064eff423d977e34e5b58fca75b
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-04 21:43:05 +0300

    Time: 130 ms (91.38%), Space: 17.1 MB (10.07%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,8 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            if nums.count(nums[i]) == 1:
-                return nums[i]
+        return sum(list(set(nums)))*2 - sum(nums)
+#         for i in range(len(nums)):
+#             if nums.count(nums[i]) == 1:
+#                 return nums[i]
                 
                 
-        return 
+#         return 
