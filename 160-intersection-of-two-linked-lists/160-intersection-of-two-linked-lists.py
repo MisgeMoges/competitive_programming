2d6e104b41acd33e3b021859491ec6670c200a8f
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-07 14:46:53 +0300

    Time: 141 ms (99.52%), Space: 29.5 MB (67.48%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -4,28 +4,10 @@
 #         self.val = x
 #         self.next = None
 
-# class Solution:
-#     def getIntersectionNode(self, headA, headB):
-#         dummyA, dummyB = headA, headB # Take dummy nodes
-
-#         while dummyA != dummyB:
-#             dummyA = headB if dummyA is None else dummyA.next 
-#             dummyB = headA if dummyB is None else dummyB.next
-#         # Return any dummy pointer. i.e. If any intersection node is present it'll get returned else it will return None because at the end of iteration both dummy pointers will point to NULL.
-#         return dummyA 
-
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        hashSet = set()
-        currentA = headA
-        currentB = headB
-        if currentA == currentB:
-            return currentA
-        while currentA:
-            hashSet.add(currentA)
-            currentA = currentA.next
-        while currentB:
-            if currentB in hashSet:
-                return currentB
-            currentB = currentB.next
-        return None
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        a, b = headA, headB
+        while (a != b):
+            a = headB if not a else a.next
+            b = headA if not b else b.next
+        return a
