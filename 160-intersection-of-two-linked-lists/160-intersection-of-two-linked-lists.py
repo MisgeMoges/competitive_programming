68c76940e3982c0c88e977ceb1a5bfaf4565615b
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-07 14:45:22 +0300

    Time: 172 ms (40.29%), Space: 30.1 MB (18.65%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -4,28 +4,28 @@
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def getIntersectionNode(self, headA, headB):
-        dummyA, dummyB = headA, headB # Take dummy nodes
-
-        while dummyA != dummyB:
-            dummyA = headB if dummyA is None else dummyA.next 
-            dummyB = headA if dummyB is None else dummyB.next
-        # Return any dummy pointer. i.e. If any intersection node is present it'll get returned else it will return None because at the end of iteration both dummy pointers will point to NULL.
-        return dummyA 
-
 # class Solution:
-#     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-#         hashSet = set()
-#         currentA = headA
-#         currentB = headB
-#         if currentA == currentB:
-#             return currentA
-#         while currentA:
-#             hashSet.add(currentA)
-#             currentA = currentA.next
-#         while currentB:
-#             if currentB in hashSet:
-#                 return currentB
-#             currentB = currentB.next
-#         return None
+#     def getIntersectionNode(self, headA, headB):
+#         dummyA, dummyB = headA, headB # Take dummy nodes
+
+#         while dummyA != dummyB:
+#             dummyA = headB if dummyA is None else dummyA.next 
+#             dummyB = headA if dummyB is None else dummyB.next
+#         # Return any dummy pointer. i.e. If any intersection node is present it'll get returned else it will return None because at the end of iteration both dummy pointers will point to NULL.
+#         return dummyA 
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        hashSet = set()
+        currentA = headA
+        currentB = headB
+        if currentA == currentB:
+            return currentA
+        while currentA:
+            hashSet.add(currentA)
+            currentA = currentA.next
+        while currentB:
+            if currentB in hashSet:
+                return currentB
+            currentB = currentB.next
+        return None
