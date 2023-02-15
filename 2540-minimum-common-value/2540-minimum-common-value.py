1050fcb33da9fe2d0df278223dfce89c2f7e2962
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-15 13:55:41 +0300

    Time: 468 ms (80.42%), Space: 39.5 MB (25.83%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,11 +1,9 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1 = set(nums1)
-        nums2 = set(nums2)
-        intersect = nums1.intersection(nums2)
+       
+        intersect =list(set( nums1).intersection(set(nums2)))
         if len(intersect) == 0:
             return -1
-        intersect = list(intersect)
         minValue = intersect[0]
         for i in range(1, len(intersect)):
             minValue = min(minValue, intersect[i])
