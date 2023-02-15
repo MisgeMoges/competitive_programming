685825720b044df7811546e5631a2d27b4d46364
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-15 17:25:27 +0300

    Time: 41 ms (25.03%), Space: 14 MB (8.31%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
@@ -1,11 +1,11 @@
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
-        nums = sorted(nums)
         if len(nums) == 0:
             return -1
-        right = len(nums)-1
+      
         ans = []
-        for i in range(len(nums)):
-            ans.append((nums[i] + nums[right])/2)
-            right -= 1
+        for i in range(len(nums)//2):
+            ans.append((max(nums)+min(nums))/2)
+            nums.remove(max(nums))
+            nums.remove(min(nums))
         return len(set(ans))
