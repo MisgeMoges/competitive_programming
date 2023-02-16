6115e11528de6fd78f9f92ca6168b5525197ab81
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-16 16:10:55 +0300

    Time: 41 ms (20.24%), Space: 13.8 MB (96.47%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,21 +1,14 @@
-# class Solution:
-#     def reversePrefix(self, word: str, ch: str) -> str:
-#         left, right = 0, 0
-#         word = list(word)
-#         while right < len(word):
-#             if ch == word[right]:
-#                 while left < right and right > 0:
-#                     word[left], word[right] = word[right], word[left]
-#                     right -= 1
-#                     left += 1
-#                 return "".join(word)
-#             right += 1
-#         return "".join(word)
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        try:
-            idx = word.index(ch)
-            w = word[:idx + 1]
-            return w[::-1] + word[idx + 1:]
-        except: return word
+        left, right = 0, 0
+        word = list(word)
+        while right < len(word):
+            if ch == word[right]:
+                while left < right and right > 0:
+                    word[left], word[right] = word[right], word[left]
+                    right -= 1
+                    left += 1
+                return "".join(word)
+            right += 1
+        return "".join(word)
         
