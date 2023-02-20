4ae0d687233e075ce3a5c8f6af8bce9ac28388eb
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-20 11:41:11 +0300

    Time: 217 ms (57.82%), Space: 15.3 MB (90.23%) - LeetHub

---
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        hashSet = set()
+        for num in nums:
+            if num in hashSet:
+                return num
+            hashSet.add(num)
+        return -1
+    
+            
+        
