8ff8272a1be6265a78dbd9cb5a08899822bd3746
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-02-26 18:21:12 +0300

    Time: 110 ms (95.86%), Space: 14.5 MB (37.38%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        height_dict=dict(zip(heights, names))
+        names = []
+        for key in sorted(height_dict.keys(), reverse = True):
+            names.append(height_dict[key])
+        return names
+            
