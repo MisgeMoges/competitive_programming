cccf96e459f28c92e2dce0c34c35da589945652a
Misgan Moges <110138721+MisgeMoges@users.noreply.github.com>
2023-07-28 16:24:57 +0300

    Create print_function.py

---
+++ print_function.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    # def sum(n):
+    #     sum = ''
+    #     for i in range(1, n+1):
+    #      sum += i 
+    #     return sum
+    # sum(n)
+    
+    for i in range(1, n+1):
+        print(i, end='')
