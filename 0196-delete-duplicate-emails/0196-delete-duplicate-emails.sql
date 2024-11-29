c9e7d885d616217ed9b42867af7ebd3f7e83a3ca
Misgan Moges <misgewsatlmay@gmail.com>
2024-11-29 10:28:47 +0300

    Time: 646 ms (86.75%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -5,16 +5,16 @@
 #     FROM Person
 #     GROUP BY email
 # )
-# WITH CTE AS (
-#     SELECT id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
-#     FROM Person
-# )
-# DELETE FROM Person
-# WHERE id IN (
-#     SELECT id
-#     FROM CTE
-#     WHERE rn > 1
-# );
+WITH CTE AS (
+    SELECT id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+    FROM Person
+)
+DELETE FROM Person
+WHERE id IN (
+    SELECT id
+    FROM CTE
+    WHERE rn > 1
+);
 
-DELETE p1 FROM Person p1, Person p2
-WHERE p1.email = p2.email and p1.id > p2.id
+# DELETE p1 FROM Person p1, Person p2
+# WHERE p1.email = p2.email and p1.id > p2.id
