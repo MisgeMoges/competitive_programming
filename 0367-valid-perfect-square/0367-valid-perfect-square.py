603c01e8208873ce2d92da8f3343cabea613a24d
Misgan Moges <misgewsatlmay@gmail.com>
2024-12-03 09:32:12 +0300

    Time: 0 ms (100.00%), Space: 17.1 MB (6.30%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,11 +1,25 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        i = 1
-        while i*i <= num:
-            result = i*i
-            if result == num:
+        # i = 1
+        # while i*i <= num:
+        #     result = i*i
+        #     if result == num:
+        #         return True
+        #     i+=1
+        if num < 1:
+            return False
+        
+        left, right = 1, num
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            if square == num:
                 return True
-            i+=1
+            elif square < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
         return False
         
         
