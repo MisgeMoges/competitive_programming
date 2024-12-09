ddd9a2a6fa02147905b6edf0ccbb80a3a48d97ba
Misgan Moges <misgewsatlmay@gmail.com>
2024-12-09 20:47:34 +0300

    Time: 19 ms (58.16%), Space: 18.5 MB (5.08%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.py
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -1,17 +1,12 @@
-
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        if not timeSeries:  # Handle empty list case
+        if not timeSeries:
             return 0
-        
         total_time = 0
-
         for i in range(1, len(timeSeries)):
-            # Add the minimum of the gap or the poison duration
-            total_time += min(duration, timeSeries[i] - timeSeries[i - 1])
-
-        # Add the duration for the last attack
+            overlap_time = timeSeries[i]-timeSeries[i-1]
+            total_time += min(overlap_time, duration)
+            
         total_time += duration
-
         return total_time
         
