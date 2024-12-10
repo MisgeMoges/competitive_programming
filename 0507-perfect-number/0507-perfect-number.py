f17efcbb0635f4907e5ce237d093bb3fb51de5b9
Misgan Moges <misgewsatlmay@gmail.com>
2024-12-10 11:36:38 +0300

    Time: 7 ms (30.89%), Space: 17.3 MB (13.63%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -2,14 +2,21 @@ class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         if num <= 1:
             return False
-        divisor = 2
+        # divisor = 2
         divisor_sum = 1
-        while divisor <= (int(math.sqrt(num))):
-            if num % divisor == 0:
-                divisor_sum += divisor
-                if divisor != num//divisor:
-                    divisor_sum += num//divisor
+        limit = int(math.sqrt(num))
+        for i in range(2, limit+1):
+            if num % i == 0:
+                divisor_sum += i
+                if i != num//i:
+                    divisor_sum += num//i
+                
+#         while divisor <= (int(math.sqrt(num))):
+#             if num % divisor == 0:
+#                 divisor_sum += divisor
+#                 if divisor != num//divisor:
+#                     divisor_sum += num//divisor
             
-            divisor += 1
+#             divisor += 1
        
         return divisor_sum == num
