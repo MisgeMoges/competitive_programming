14667fb13db23308378c03ca1390d6d4cb941e98
Misgan Moges <misgewsatlmay@gmail.com>
2024-12-13 17:29:38 +0300

    Time: 14 ms (88.05%), Space: 18.9 MB (19.23%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,9 +1,13 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        candy_type = set()
-        for i in range(len(candyType)):
-            if candyType[i] not in candy_type:
-                candy_type.add(candyType[i])
-            if len(candyType)//2 < len(candy_type):
-                return len(candyType)//2
-        return len(candy_type)
+        
+        length = len(set(candyType))
+        max_candies = len(candyType)//2
+        return min(length, max_candies)
+        # candy_type = set()
+        # for i in range(len(candyType)):
+        #     if candyType[i] not in candy_type:
+        #         candy_type.add(candyType[i])
+        #     if len(candyType)//2 < len(candy_type):
+        #         return len(candyType)//2
+        # return len(candy_type)
