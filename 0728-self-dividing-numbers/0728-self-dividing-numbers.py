03ef87b8960f2a0af5c4b3dcbb0f91007c7230ae
Misgan Moges <misgewsatlmay@gmail.com>
2024-12-20 20:17:16 +0300

    Time: 3 ms (97.77%), Space: 17.9 MB (5.26%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def selfDividingNumbers(self, left, right):
+        def is_self_dividing(num):
+            original = num
+            while num > 0:
+                digit = num % 10
+                if digit == 0 or original % digit != 0:
+                    return False
+                num //= 10
+            return True
+
+        result = []
+        for num in range(left, right + 1):
+            if is_self_dividing(num):
+                result.append(num)
+
+        return result
